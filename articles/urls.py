5edc5c4dc4ef500806a7a4974efff34fc358f6a5
JeesooHaa <haajeesoo@gmail.com>
2019-09-11 16:01:42 +0900

    Finish django_review

---
+++ articles/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+from . import views
+
+# domain.com/articles/__
+urlpatterns = [
+    path('', views.index),
+    path('new/', views.new),
+    path('create/', views.create),
+    path('<int:article_pk>/delete/', views.delete),
+    path('<int:article_pk>/edit/', views.edit),
+    path('<int:article_pk>/update/', views.update),
+]
